ebd7284c6bd0effed60046c24e294ff32ac59807
DC-21 <cholahkuboko@gmail.com>
2023-10-22 23:17:11 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/power/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.power
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Ai Scanner"),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () async {
+              final result =
+                  await FilePicker.platform.pickFiles(allowMultiple: true);
+              if (result == null) return;
+
+              final file = result.files.first;
+              final newFile = await saveFilePermanently(file);
+              //openFile(file);
+
+              print('Name:${file.name}');
+              print('Bytes:${file.bytes}');
+              print('Size:${file.size}');
+              print('Extension:${file.extension}');
+              print('Path:${file.path}');
+
+              print('From Path: ${file.path!}');
+              print('To Path: ${newFile.path}');
+            },
+            child: const Text("Select Files"),
+          ),
+        ));
+  }
+
+  Future<void> saveFilePermanently(PlatformFile file) async {
+    final appStorage = await getApplicationDocumentsDirectory();
+    final newFile = File('${appStorage.path}/${file.name}');
+    await File(file.path!).copy(newFile.path);
+  }
+
+  void openFile(PlatformFile file) {
+    OpenFile.open(file.path!);
+  }
+}
